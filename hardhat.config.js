f630bfe67c9fb0a8708b9e4ed6ffc5b8101de411
MuntahaFatima14 <muntahaf81@gmail.com>
2026-01-31 00:30:01 +0500

    edited

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:8545",
+      // Ganache automatically provides accounts, 
+      // so you don't strictly need to paste private keys here.
+    }
+  }
 };
